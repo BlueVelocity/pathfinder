55079b9b124161f5ef3ac86bb4ffa577742e7db5
BlueVelocity <josephrodney@hotmail.com>
2024-07-28 12:16:24 -0400

    Set overall layout, show translucent background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <div className="flex-1 flex flex-col align-center"></div>;
 }
 
 export default App;
