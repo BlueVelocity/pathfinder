69d936a78706f45380befb1e2e86595bd057a5a7
BlueVelocity <josephrodney@hotmail.com>
2024-07-30 15:37:58 -0400

    Add Nunito font

---
+++ src/components/DropMenu.tsx
@@ -0,0 +1,3 @@
+export default function DropMenu() {
+  return <div className="flex-1 rounded"></div>;
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,8 @@ function Header() {
     <div className="sticky top-2 flex bg-primary w-full max-h-16 rounded mb-4 px-3 text-lg text-accent">
       <img className="max-h-12" src={home} alt="" />
       <button className="ml-3 active:text-black text-xl">ByteTrail</button>
-      <button className="mr-2 ml-auto active:text-black">About</button>
+      <button className="mr-2 ml-auto active:text-black">Projects</button>
+      <button className="mr-2 ml-3 active:text-black">About</button>
     </div>
   );
 }
