897eb3fbcd4fd48518d59751806f9785b75dd03e
BlueVelocity <josephrodney@hotmail.com>
2024-07-29 21:26:26 -0400

    Change site name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="sticky top-2 flex bg-primary w-full max-h-16 rounded mb-4 px-3 text-lg text-accent">
       <img className="max-h-12" src={home} alt="" />
-      <button className="ml-3 active:text-black text-xl">Pathfinder</button>
+      <button className="ml-3 active:text-black text-xl">ByteTrail</button>
       <button className="mr-2 ml-auto active:text-black">About</button>
     </div>
   );
